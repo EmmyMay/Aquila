31c2ce5750b732a553ba1115967bf19fb362c1f6
EmmyMay <lekeemmy@gmail.com>
2021-01-08 00:04:08 +0100

    switched up the design and initial completion

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,93 @@
 <template>
-  <div class="home">
+  <div class="home flex flex-col">
     <div class="home_wrapper grid grid-cols-2">
-      <div class="home_dark justify-end flex flex-col bg-aquiladark">
-        <ul class="home_menus1 w-9/12 flex justify-evenly">
-          <li v-for="(menu, i) in menus" :key="i" class="home_menu text-white">
-            {{ menu }}
-          </li>
-          <li><img class="home_avatar" src="../assets/avatar.png" alt=""></li>
-        </ul>
-        
+      <div class="home_dark justify-end flex flex-col bg-aquiladark"></div>
+      <div class="home_light bg-aquilalight flex flex-col"></div>
+    </div>
+    <div class="home__wrapper flex flex-col">
+      <h1
+        class="home_title text-5xl font-display font-bold opacity-60 text-white"
+      >
+        {{name}}
+      </h1>
+      <div class="home_imgbox flex justify-center items-end">
+        <img src="../assets/mainimg.png" alt="" class="home_mainimg" />
       </div>
-      <div class="home_light bg-aquilalight flex flex-col">
-        <h1 class="home_title text-5xl font-display font-bold opacity-60 text-white " >AQUILA AVEION</h1>
-        <div class="home_imgbox flex justify-center items-end  ">
-          <img src="../assets/mainimg.png" alt="" class="home_mainimg">
-        </div>
+      <div class="menu_wrapper flex">
+        <ul class="home_menus1 w-9/12 flex justify-evenly">
+          <li
+            v-for="(menu, i) in menus"
+            :key="i"
+            class="home_menu opacity-75 font-display font-light text-white"
+          >
+            {{ menu }}
+          </li>
+          <li><img class="home_avatar" src="../assets/avatar.png" alt="" /></li>
+        </ul>
         <ul class="home_menus2 w-9/12 flex justify-evenly">
-          <li v-for="(menu, i) in menus2" :key="i" class="home_menu text-white">
+          <li
+            v-for="(menu, i) in menus2"
+            :key="i"
+            class="home_menu opacity-75 font-display font-light text-white"
+          >
             {{ menu }}
           </li>
         </ul>
+      </div>
+      <div class="home_socials self-end ">
+        <a v-for="link in socials" :key="link.name" :href="`https://${link.name}.com/${socialmedia[link.name]}`" class="home_links">
+          <img :src="getUrl(link.icon)" :alt="link.name" class="home_icons">
+        </a>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  // @ is an alias to /src
+ import axios from 'axios';
 
   export default {
     name: "Home",
 
     data() {
       return {
+        name: ''|| 'loading...',
         menus: ["HOME", "ABOUT"],
         menus2: ["GALLERY", "CONTACT"],
-      };
+        socials:[
+          {name: 'instagram', icon: 'instagram.svg', },
+          {name: 'twitter', icon: 'twitter.svg', },
+          {name: 'snapchat', icon: 'snapchat.svg',  },
+          {name: 'email', icon: 'email.svg',},
+        ],
+        socialmedia: {
+          email: '',
+          instagram: '',
+          snapchat: '',
+          twitter: ''
+        } || 'loading',
+        arrived: false
+      }
     },
+  async  mounted () {
+      let res =  await axios.get('https://hirng-X2021.glitch.me/api', {
+        headers:{
+          'Access-Control-Allow-Origin' : '*',
+        }
+      });
+      this.name = res.data.name;
+      this.socialmedia = res.data.social_media;
+      console.log(this.socialmedia);
+
+    },
+
+    methods: {
+       getUrl(url) {
+        return require("../assets/" + url);
+      }
+
+    }
+  
   };
 </script>
 
@@ -52,12 +103,33 @@
   height: 100%;
 }
 
+.home__wrapper {
+  position: absolute;
+
+  height: 100%;
+  width: 80%;
+  top: 0;
+  left: 20%;
+}
+.home_title {
+  margin: 0 auto;
+  position: relative;
+  right: 11%;
+  top: 2rem;
+}
+.menu_wrapper {
+  position: relative;
+  top: 24%;
+  height: 3rem;
+}
+
 .home_imgbox {
   position: relative;
-  right: 10rem;
-  top: 24%;
-  height: 20rem;
-  width: 20rem;
+  margin: 0 auto;
+  right: 13%;
+  top: 8%;
+  height: 30rem;
+  width: 30rem;
   background-image: linear-gradient(
     to right,
     #cb2964,
@@ -66,27 +138,39 @@
     #c11753,
     #bd0f4d
   );
+ -webkit-box-shadow: -2px 3px 24px -7px rgba(50, 50, 50, 0.75);
+-moz-box-shadow:    -2px 3px 24px -7px rgba(50, 50, 50, 0.75);
+box-shadow:         -2px 3px 24px -7px rgba(50, 50, 50, 0.75);
 }
 .home_menus1 {
   position: relative;
   bottom: 2rem;
-  left: 22rem;
+  height: fit-content;
 }
 .home_menus2 {
   position: relative;
-  top: 19.3rem;
+  bottom: 2rem;
+  right: 12rem;
 }
-.home_avatar{
+.home_avatar {
   object-fit: contain;
   position: relative;
   bottom: 2rem;
-  
 }
 
-.home_mainimg{
+.home_mainimg {
   height: 80%;
   width: 80%;
   object-fit: cover;
-  
+}
+.home_socials{
+  position: relative;
+  bottom: 40%;
+  right: 5%;
+}
+.home_icons{
+  width: 5rem;
+  height: 5rem;
+  margin-bottom: 2rem;
 }
 </style>
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+// module.exports = {
+//     devServer: {
+//         proxy: 'https://hiring-X2021.glitch.me/'
+//     }
+// }
